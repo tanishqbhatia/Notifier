829d421a5a3caa7cbb9394bb232cdf0730b66bf1
Tanishq Bhatia <crash0er@gmail.com>
2018-04-25 17:52:21 +0530

    Initial commit

---
+++ app/src/main/java/in/co/notifier/activities/introduction/SplashActivity.java
@@ -0,0 +1,15 @@
+package in.co.notifier.activities.introduction;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.co.notifier.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
