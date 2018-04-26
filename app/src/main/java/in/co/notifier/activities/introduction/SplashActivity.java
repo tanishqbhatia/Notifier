87da256fd9f50deeedb274c6999fd5185aed167e
Tanishq Bhatia <crash0er@gmail.com>
2018-04-26 18:11:36 +0530

    Added appintro for introduction

--- app/src/main/java/in/co/notifier/activities/introduction/SplashActivity.java
+++ app/src/main/java/in/co/notifier/activities/introduction/SplashActivity.java
@@ -1,7 +1,8 @@
 package in.co.notifier.activities.introduction;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import in.co.notifier.R;
 
@@ -11,5 +12,6 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        startActivity(new Intent(this, IntroductionActivity.class));
     }
 }
