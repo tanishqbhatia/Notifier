46e896d461ea6120f9c7bda879bf5fcd416df9a3
Tanishq Bhatia <crash0er@gmail.com>
2018-04-30 11:56:51 +0530

    Added Introduction activity

--- app/src/main/java/in/co/notifier/activities/introduction/IntroductionActivity.java
+++ app/src/main/java/in/co/notifier/activities/introduction/IntroductionActivity.java
@@ -1,16 +1,12 @@
 package in.co.notifier.activities.introduction;
 
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import in.co.notifier.R;
-import in.co.notifier.activities.introduction.parallax_viewpager.ParallaxViewPager;
-import in.co.notifier.adapters.IntroductionViewPagerAdapter;
+
 
 public class IntroductionActivity extends AppCompatActivity {
-
-    private ParallaxViewPager introductionVp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,32 +14,14 @@ public class IntroductionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_introduction);
         findViews();
         addListeners();
-        introductionVp.addMovementToView(R.id.appNameTv, 0.8f);
-        introductionVp.addMovementToView(R.id.introImageIv, 0.0f);
-        introductionVp.addMovementToView(R.id.continueLoginBtn, 0.0f);
-        introductionVp.setAdapter(new IntroductionViewPagerAdapter(getLayoutInflater()));
+
     }
 
     private void addListeners() {
-        introductionVp.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
     }
 
     private void findViews() {
-        introductionVp = findViewById(R.id.introductionVp);
+
     }
 }
